5d70d8e5f6403a156c92fcc5c83349bdbb2da47d
laulun98 <lpajarinalonso@gmail.com>
2025-11-14 18:14:15 +0100

    a

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <MenuBar/>
+  
 </template>
 
 <script setup>
--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
     <h1>Home</h1>
+    <prueba/>
 </template>
 
 <script setup>
-
+import prueba from './prueba.vue';
 </script>
