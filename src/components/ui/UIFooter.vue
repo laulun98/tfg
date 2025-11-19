bf522a61b30610fc9e5b24adac0a8eb5bc065b46
laulun98 <lpajarinalonso@gmail.com>
2025-11-19 15:59:44 +0100

    UI footer cambiado, estilo support us modificado, footer añadido en app.vue

--- src/components/ui/UIFooter.vue
+++ src/components/ui/UIFooter.vue
@@ -1,6 +1,23 @@
 <template>
     <div class="ui-footer">
-        <h1>Footer</h1>
+        <div style="display: flex; justify-content: center; gap:100px">
+            <div>
+            <h2>Empresa</h2>
+            <ul>
+                <li><RouterLink to="/home">Home</RouterLink></li>
+                <li><RouterLink to="/about">About Us</RouterLink></li>
+                <li><RouterLink to="/games">Games</RouterLink></li>
+            </ul>
+        </div>
+        <div>
+            <h2>Support</h2>
+            <ul>
+                <li><RouterLink to="/support">Support Us</RouterLink></li>
+                <li><RouterLink to="/contact">Contact Us</RouterLink></li>
+                <li>Política de privacidad</li>
+            </ul>
+        </div>
+        </div>
     </div>
 </template>
 
