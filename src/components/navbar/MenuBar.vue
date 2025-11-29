32d4d8c79df2f04c13deb446f864bf395c891867
laulun98 <lpajarinalonso@gmail.com>
2025-11-29 16:19:42 +0100

    menubar

--- src/components/navbar/MenuBar.vue
+++ src/components/navbar/MenuBar.vue
@@ -1,23 +1,25 @@
 <template>
-    <div style="width: 100%">
-        <nav style="display: flex; justify-content: center; gap: 5px;">
-            <div class="links">
-                <RouterLink to="/home">Home</RouterLink>
-            </div>
-            <div class="links">
-                <RouterLink to="/about">About Us</RouterLink>
-            </div>
-            <div class="links">
-                <RouterLink to="/support">Support Us</RouterLink>
-            </div>
-            <div class="links">
-                <RouterLink to="/games">Games</RouterLink>
-            </div>
-            <div class="links">
-                <RouterLink to="/contact">Contact Us</RouterLink>
-            </div>
-        </nav>
-        <RouterView />
+    <div>
+        <div style="width: 100%;">
+            <nav style="display: flex; justify-content: center; gap: 5px;">
+                <div class="links">
+                    <RouterLink to="/home">Home</RouterLink>
+                </div>
+                <div class="links">
+                    <RouterLink to="/about">About Us</RouterLink>
+                </div>
+                <div class="links">
+                    <RouterLink to="/support">Support Us</RouterLink>
+                </div>
+                <div class="links">
+                    <RouterLink to="/games">Games</RouterLink>
+                </div>
+                <div class="links">
+                    <RouterLink to="/contact">Contact Us</RouterLink>
+                </div>
+            </nav>
+            <RouterView />
+        </div>
     </div>
 </template>
 
