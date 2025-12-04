b07d231e14a6be7dc6787a4d20b9ecb02bed5b62
laulun98 <lpajarinalonso@gmail.com>
2025-12-04 17:20:45 +0100

    arreglado que no arraque la web desde home

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -2,9 +2,10 @@
     <PageHeader title="Home" :bannerStyle="'banner'"/>
     <div class="home-content">
         <h1>Aftos es un estudio madrileño de desarrollo de videojuegos. <br>Creando "Afesis"</h1>
-        <P>Somos un estudio de videojuegos y creación audiovisual que advoca por la cultura, el talento de jóvenes creadores y las historias que inspiran. <br><RouterLink to="/about" class="red-link">Leer más</RouterLink></P>
+        <P>Somos un estudio de videojuegos y creación audiovisual que advoca por la cultura, el talento de jóvenes creadores y las historias que inspiran. 
+        <br><RouterLink to="/about" class="yellow-link">Leer más</RouterLink></P>
         <br>
-        <RouterLink to="/games" class="red-link">Presskit de Afesis</RouterLink>
+        <RouterLink to="/games" class="yellow-link">Presskit de Afesis</RouterLink>
         <SocialButton/>
     </div>
 </template>
