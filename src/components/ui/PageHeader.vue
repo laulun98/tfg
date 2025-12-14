cb423f63a18c4dd36749f21d2d477a3af16ae146
laulun98 <lpajarinalonso@gmail.com>
2025-12-14 22:02:59 +0100

    banner cambia según la hora

--- src/components/ui/PageHeader.vue
+++ src/components/ui/PageHeader.vue
@@ -1,24 +1,35 @@
 <template>
-    <div :class=bannerStyle>
+    <div :class=finalBannerClass>
         <h1 style="background-color: rgba(71, 52, 52, 0.4); padding:5px; border-radius: 80%; backdrop-filter: blur(1.5px);"> {{ title }}</h1>
     </div>
 </template>
 
 <script setup>
+import { computed } from 'vue';
+const hour = new Date().getHours();
+console.log(hour)
 
 const props = defineProps({
     /*title: {
         type: String,
         default: ''
-    }, */
+    }, 
     imgSrc: {
         type: String,
         default: 'assets/imgs/banner.jpeg'
-    }, 
+    }, */
     bannerStyle: {
         type: String,
         default: 'secondary_banner' //puede ser banner (para el home) o secondary_banner (para el resto)
     }
 })
+
+const finalBannerClass = computed(() => {
+  if (hour.value) {
+    return props.bannerStyle
+  }
+  return `${props.bannerStyle}_night`
+})
+
 
 </script>
