960389795cc464a7621164faca0ff619aa94278f
laulun98 <lpajarinalonso@gmail.com>
2026-01-16 16:38:16 +0100

    cambios traduccion

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div style="border: 2px">
-    <MenuBar/>
+    <MenuBar :page="route.path"/>
   </div>
   <UIFooter/>
 </template>
 
 <script setup>
+import { watch } from 'vue';
 import MenuBar from './components/navbar/MenuBar.vue';
 import UIFooter from './components/ui/UIFooter.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
 
 </script>
