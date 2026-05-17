5ab83f5335cf31fe938dfca52f3d3e7a15a8a80c
Pau3Duskflowerz <paulamurilloalonso@gmail.com>
2026-05-17 19:38:30 +0200

    more images and some css changes

--- src/components/navbar/MenuBar.vue
+++ src/components/navbar/MenuBar.vue
@@ -2,7 +2,7 @@
     <div style="width: 100%; margin-bottom: 5px;">
         <nav style="display: flex; justify-content: center; gap: 5px; position:sticky; top:0;z-index: 1000;">
             <div style="margin:6px">
-                <RouterLink to="/home"><img :src="'assets/imgs/logo-menu.png'" :alt="logo" style="height:30px; width:30px" /></RouterLink>
+                <RouterLink to="/home"><img :src="'assets/imgs/LogoDusk.PNG'" :alt="logo" style="height:30px; width:35px" /></RouterLink>
             </div>
             <div class="links">
                 <RouterLink to="/home">Home</RouterLink>
